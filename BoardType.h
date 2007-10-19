030921cb77821499fda7fbfc67273af8b8442cd5
bvr <bvr@c770f1bc-0775-48ed-bd74-6edc8452e29f>
2007-10-19 17:49:55 +0000

    This is an initial commit.  This version is a hacked up version from CAFE. My intent with this package is to completely separate the clustering algorithm from CAFE into its own package for distribution. Many of these files will disappear from this package and will be a part of CAFE.
    git-svn-id: svn+ssh://localhost/home/bvr/svnrepos/Programs/SPAnalysis@37 c770f1bc-0775-48ed-bd74-6edc8452e29f

---
+++ BoardType.h
@@ -0,0 +1,39 @@
+#ifndef _BOARDTYPE_H
+#define _BOARDTYPE_H
+
+#include <vector>
+#include <string>
+
+#include "ClusterConfig.h"			// for ClusterConfig class
+
+
+class BoardType
+{
+	public:
+		BoardType();
+
+		void AddMember(const LonLatAnomDate &AMember);
+
+		bool SpecialStrongPointRule(const ClusterConfig &TheConfig) const;
+		bool ShouldIgnore(const ClusterConfig &TheConfig) const;
+		bool ShouldIgnore(const ClusterConfig &TheConfig, const double DistFromCheck) const;
+
+//		string MemberToString(const size_t MemberIndex) const;
+
+		int MemberCount() const;
+		vector <LonLatAnomDate> ReturnMembers() const;
+
+		double GiveValue() const;
+		LonLatAnomDate ReturnMember(const size_t &MemberIndex) const;
+		
+		void Reset();
+
+	private:
+		vector <LonLatAnomDate> myMembers;
+
+	friend void operator += (BoardType &Lefty, const BoardType &Righty);
+	friend bool operator == (const BoardType &Lefty, const BoardType &Righty);
+	friend bool operator != (const BoardType &Lefty, const BoardType &Righty);
+};
+
+#endif
